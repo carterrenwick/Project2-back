43b0ee5d91a386ba3a8dee18628de6f9f4996066
Carter Taylor <carterrenwick@outlook.com>
2018-01-21 15:58:24 -0500

    changing port back to 80

--- Project2-back/src/main/java/com/revature/rest/CreateCtrl.java
+++ Project2-back/src/main/java/com/revature/rest/CreateCtrl.java
@@ -3,18 +3,15 @@ package com.revature.rest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.model.Board;
-import com.revature.model.Card;
-import com.revature.model.SwimLane;
+import com.revature.model.Task;
 import com.revature.service.BoardServiceContract;
 import com.revature.service.CardServiceContract;
 import com.revature.service.SwimLaneServiceContract;
-import com.revature.model.Task;
 import com.revature.service.TaskServiceContract;
 
 @RestController
@@ -26,8 +23,9 @@ public class CreateCtrl {
 	@GetMapping("/createTask/{cId}&{description}")
 	public Task testCreateTask(@PathVariable int cId, @PathVariable String description) {
 		return taskService.createTask(cId, description);
-		
-  @Autowired
+	}
+	
+	@Autowired
 	BoardServiceContract boardSer;
 	
 	@PostMapping("/createBoard")

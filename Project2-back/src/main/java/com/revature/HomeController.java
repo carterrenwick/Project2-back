24486bb3eb990bf799f4b2d7c879d7a14db63207
Carter Taylor <carterrenwick@outlook.com>
2018-01-30 23:00:52 -0500

    LAST MINUTE

---
+++ Project2-back/src/main/java/com/revature/HomeController.java
@@ -0,0 +1,15 @@
+package com.revature;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+@RequestMapping("/home")
+public class HomeController {
+    @RequestMapping(method = RequestMethod.GET)
+    public String index() {
+        return "index.html";
+    }
+}
